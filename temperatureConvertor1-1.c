c94de656131595b6d4df71af093a528022d69e94
ipulepipi <ipulepipi@nilexia.com>
2024-06-03 21:02:54 +1000

    convert to floating type

--- temperatureConvertor1-1.c
+++ temperatureConvertor1-1.c
@@ -6,19 +6,19 @@ for fahr = 0, 20, ..., 300*/
 int main() {
     
 
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0; /*lower limit of temperture scale*/
     upper = 300; /*upper limit*/
     step = 20;
     
-    printf("Fahrenheit to Celcius");
+    printf("Fahrenheit to Celcius\n\n");
     fahr = lower;
     while (fahr <= upper) 
     {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3d\t%6d\n", fahr, celsius); //added 3d to make it 3 digit wide and add 6d to make it 6 digit wide
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius); //added 3d to make it 3 digit wide and add 6d to make it 6 digit wide
         fahr = fahr + step;
     }
     
